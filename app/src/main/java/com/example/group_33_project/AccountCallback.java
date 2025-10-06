9327d63eab670b183a23394e0575d32616b38a5d
Colin Gordon <cgord046@uottawa.ca>
2025-10-06 13:17:22 -0400

    Clarifying the usage of callbacks

--- app/src/main/java/com/example/group_33_project/AccountCallback.java
+++ app/src/main/java/com/example/group_33_project/AccountCallback.java
@@ -1,5 +1,9 @@
 package com.example.group_33_project;
 
+// we must use callbacks because of the asynchronous behaviour of the firebase database -> if we try to throw exceptions or use other methods, it would freeze the main thread!
+// thus we use callbacks to allow the main thread to run smoothly, and then receive the data when the queries are complete :)
+
+// the onSuccess and onFailure methods must be defined for each usage, as it depends on the use case!
 public interface AccountCallback { // to handle issues regarding SIGN UP/SIGN IN
     void onSuccess(String msg);
     void onFailure(String msg);
--- app/src/main/java/com/example/group_33_project/QueryCallback.java
+++ app/src/main/java/com/example/group_33_project/QueryCallback.java
@@ -1,5 +1,9 @@
 package com.example.group_33_project;
 
+// we must use callbacks because of the asynchronous behaviour of the firebase database -> if we try to throw exceptions or use other methods, it would freeze the main thread!
+// thus we use callbacks to allow the main thread to run smoothly, and then receive the data when the queries are complete :)
+
+// the onSuccess and onFailure methods must be defined for each usage, as it depends on the use case!
 public interface QueryCallback { // making a callback that will allow us to handle errors during QUERYING EMAILS(i.e. onSuccess means email FOUND, else there was no email associated)
         void onSuccess(Account account);
         void onFailure(String errorMessage);
