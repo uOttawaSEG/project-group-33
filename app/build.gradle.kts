731848fb64f6e1c0a6401d3ed8ce5fc89022c02a
Diraclol <danielnguyen@tutamail.com>
2025-11-07 17:14:26 -0500

    - Changing gradle builds to work with Circleci - Changing config files again

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.group_33_project"
-    compileSdk = 36
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.group_33_project"
         minSdk = 26
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
