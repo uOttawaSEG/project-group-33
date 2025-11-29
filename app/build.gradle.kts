b5bf8faa16c41eba9bfb1e3a598871cd58436ea7
Colin Gordon <cgord046@uottawa.ca>
2025-11-29 14:16:08 -0500

    upping minimum SDK version (for certain time methods to work)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.group_33_project"
-        minSdk = 26
+        minSdk = 31
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
